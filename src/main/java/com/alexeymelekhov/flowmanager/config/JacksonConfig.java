813111819641ff3cd98d424df90ef26226e0e54e
leshasmp <leshasmp1989@mail.ru>
2026-09-10 12:28:44 +0500

    feat: add config

---
+++ src/main/java/com/alexeymelekhov/flowmanager/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package com.alexeymelekhov.flowmanager.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
