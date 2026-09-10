6ed3b24e39e8e62631f5e9cb942494276faad1cc
leshasmp <leshasmp1989@mail.ru>
2026-09-10 12:24:03 +0500

    chore: initialize Spring Boot project

---
+++ src/main/java/com/alexeymelekhov/flowmanager/FlowManagerApplication.java
@@ -0,0 +1,17 @@
+package com.alexeymelekhov.flowmanager;
+
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class FlowManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowManagerApplication.class, args);
+    }
+
+}
